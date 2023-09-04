20b44262787ad4866fd3461023e8065867d28ea1
ziyuwang <wangziyu@seas.upenn.edu>
2023-09-04 16:22:42 -0400

    solved

---
+++ hw0/hw0/src/cis5550/HelloWorld.java
@@ -0,0 +1,8 @@
+package cis5550;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+        String fullName = "Ziyu Wang";
+        System.out.println("Hello World (" + fullName + ")");
+    }
+}
