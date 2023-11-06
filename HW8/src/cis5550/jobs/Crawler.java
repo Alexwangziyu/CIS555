c01c6ef9c9d41315367aa11c63519f10086af5cf
ziyuwang <wangziyu@seas.upenn.edu>
2023-11-06 03:26:14 -0500

    v1

--- HW8/src/cis5550/jobs/Crawler.java
+++ HW8/src/cis5550/jobs/Crawler.java
@@ -114,8 +114,8 @@ public class Crawler {
         if (args.length != 1) {
             context.output("Error: Provide a single seed URL as an argument.");
         } else {
-        	
-            context.output("OK111122");
+
+            context.output("OK");
             String url = args[0];
             context.output(url);
             List<String> urlList = new ArrayList<>();
@@ -138,7 +138,7 @@ public class Crawler {
                         Row visitrecord = context.getKVS().getRow("pt-crawl", Hasher.hash(urls));
                         Row hostrecord = context.getKVS().getRow("visit-history", Hasher.hash(seedurl[1] + seedurl[2]));
                         if (visitrecord != null) {
-                            if (visitrecord.get("respond").equals("200")) {
+                            if (visitrecord.get("responseCode").equals("200")) {
                                 return results;
                             }
                         }
@@ -172,10 +172,8 @@ public class Crawler {
                             // String[] seedurl = URLParser.parseURL(urls);
                             Row curtimerow = context.getKVS().getRow("visit-history",
                                     Hasher.hash(seedurl[1] + seedurl[2]));
-
                             long currentTime = System.currentTimeMillis();
                             long timeValue = Long.parseLong(curtimerow.get("time"));
-                            System.out.println("time:"+curtimerow.get("delay")+"-"+timeValue);
                             double delayValue = Double.parseDouble(curtimerow.get("delay"));
 
                             if ((currentTime - timeValue) <= (delayValue * 1000)) {
@@ -187,8 +185,8 @@ public class Crawler {
 
                             HttpURLConnection headconnection = (HttpURLConnection) urlObj.openConnection();
                             headconnection.setRequestMethod("HEAD");
-//                            headconnection.setRequestHEAD("cis5550-crawler");
-//                            headconnection.
+                            // headconnection.setRequestHEAD("cis5550-crawler");
+                            // headconnection.
                             headconnection.connect();
                             int headResponseCode = headconnection.getResponseCode();
 
@@ -201,6 +199,34 @@ public class Crawler {
                                 int responseCode = connection.getResponseCode();
                                 System.out.print("code" + urls + ":" + responseCode);
                                 if (responseCode == 200) {
+	                            	 seedurl = URLParser.parseURL(urls);
+	                            	 if(seedurl[0]==null ||(!seedurl[0].toLowerCase().equals("http")&&!seedurl[0].toLowerCase().equals("https"))) {
+	                            		 return results;
+	                            	 }
+	                                 int lastIndex = seedurl[3].lastIndexOf("/");
+	                                 String path = "/";
+	                                 if (lastIndex != -1) {
+	                                     path = seedurl[3].substring(0, lastIndex + 1);
+	                                 }
+	                                 if(seedurl[3]!=null &&(seedurl[3].endsWith(".jpg") ||seedurl[3].endsWith(".jpeg")||seedurl[3].endsWith(".gif")||seedurl[3].endsWith(".png")||seedurl[3].endsWith(".txt"))) {
+	                            		 return results;
+	                            	 }
+	                                 String beforepath = "";
+	                                 
+	                                 if (seedurl[2] != null) {
+	                                     beforepath = seedurl[0] + "://" + seedurl[1] + ":" + seedurl[2];
+	                                 } else {
+	                                     if (seedurl[0].toLowerCase().equals("http")) {
+	                                         beforepath = seedurl[0] + "://" + seedurl[1] + ":" + "80";
+	                                     } else if(seedurl[0].toLowerCase().equals("https")){
+	                                         beforepath = seedurl[0] + "://" + seedurl[1] + ":" + "8000";
+	                                     }else {
+	                                    	 return results;
+	                                     }
+	                                 }
+                                     
+                                    Row r = new Row(Hasher.hash(urls));
+
                                     BufferedReader reader = new BufferedReader(
                                             new InputStreamReader(connection.getInputStream()));
                                     String line;
@@ -209,35 +235,24 @@ public class Crawler {
                                         content.append(line);
                                     }
                                     reader.close();
-                                    Row r = new Row(Hasher.hash(urls));
-                                    r.put("url", urls);
-                                    r.put("respond", "" + responseCode);
-                                    r.put("page", "");
-                                    context.getKVS().putRow("pt-crawl", r);
-                                    System.out.print("add" + urls + "to pt");
-                                    // context.output(urls);
-                                    // context.output(content.toString());
-                                    results = extracturl(content.toString());
-                                    System.out.println(urls + "has" + results);
-                                    // For demonstration, we add a sample new URL
-                                    // results.add("http://example.com/new-page");
-                                    seedurl = URLParser.parseURL(urls);
 
-                                    int lastIndex = seedurl[3].lastIndexOf("/");
-                                    String path = "/";
-                                    if (lastIndex != -1) {
-                                        path = seedurl[3].substring(0, lastIndex + 1);
-                                    }
-                                    String beforepath = "";
-                                    if (seedurl[2] != null) {
-                                        beforepath = seedurl[0] + "://" + seedurl[1] + ":" + seedurl[2];
-                                    } else {
-                                        if (seedurl[0].equals("http")) {
-                                            beforepath = seedurl[0] + "://" + seedurl[1] + ":" + "80";
-                                        } else {
-                                            beforepath = seedurl[0] + "://" + seedurl[1] + ":" + "8000";
+                                    if (connection.getContentType() != null) {
+                                        r.put("contentType", connection.getContentType());
+                                        if (connection.getContentType().startsWith("text/html")) {
+                                            r.put("page", "--thisispage--");
                                         }
                                     }
+                                    if (connection.getContentLength() != -1) {
+                                        r.put("Length", ""+connection.getContentLength());
+                                    }
+
+                                    r.put("url", urls);
+                                    r.put("responseCode", "" + responseCode);
+
+                                    context.getKVS().putRow("pt-crawl", r);
+                                    System.out.print("add" + urls + "to pt");
+                                    results = extracturl(content.toString());
+                                    System.out.println(urls + "has" + results);
 
                                     List<String> updatedResults = new ArrayList<>();
                                     for (String rawurl : results) {
@@ -247,9 +262,6 @@ public class Crawler {
                                             rawurl = rawurl.substring(0, fragmentIndex);
                                         }
                                         if (rawurl.equals("")) {
-                                            // rawurl = beforepath +path;
-                                            // System.out.println(rawurl);
-                                            // updatedResults.add(rawurl);
                                             continue;
                                         }
                                         if (rawurl.startsWith("/")) {
@@ -276,14 +288,14 @@ public class Crawler {
                                 // System.out.print("redirect"+urls+"to"+headResponseCode);
                                 Row r = new Row(Hasher.hash(urls));
                                 r.put("url", urls);
-                                r.put("respond", "" + headResponseCode);
+                                r.put("responseCode", "" + headResponseCode);
                                 context.getKVS().putRow("pt-crawl", r);
                                 String redirectUrl = headconnection.getHeaderField("Location");
                                 results.add(redirectUrl);
-                                System.out.print("redirect" + urls + "to" + headResponseCode + redirectUrl);
+                                System.out.println("redirect" + urls + "to" + headResponseCode + redirectUrl);
                                 return results;
                             } else {
-                                System.out.print("error" + urls + "to" + headResponseCode);
+                                System.out.println("error" + urls + "to" + headResponseCode);
                             }
                         } catch (Exception e) {
                             e.printStackTrace();
@@ -295,6 +307,7 @@ public class Crawler {
                 e.printStackTrace();
             }
         }
+
     }
 
     public static List<String> extracturl(String URL) {
@@ -330,75 +343,74 @@ public class Crawler {
     }
 
     public static String[] getrobotstxt(String URL) {
-    	try {
-        URL urlOb = new URL(URL + "/robots.txt");
-        HttpURLConnection connection = (HttpURLConnection) urlOb.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
-        int responseCode = connection.getResponseCode();
+        try {
+            URL urlOb = new URL(URL + "/robots.txt");
+            HttpURLConnection connection = (HttpURLConnection) urlOb.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int responseCode = connection.getResponseCode();
 
-        if (responseCode == 200) {
-            // 读取 robots.txt 文件内容
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder robotstxtContent = new StringBuilder();
-            String line;
+            if (responseCode == 200) {
+                // 读取 robots.txt 文件内容
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder robotstxtContent = new StringBuilder();
+                String line;
 
-            while ((line = reader.readLine()) != null) {
-                robotstxtContent.append(line).append("\n");
-            }
-            reader.close();
-
-            // 解析 robots.txt 内容
-            String[] lines = robotstxtContent.toString().split("\n");
-            List<String> disallowRules = new ArrayList<>();
-            List<String> allowRules = new ArrayList();
-            String delay = "1";
-            boolean specificRules = false; // 是否有特定的规则适用于 "cis5550-crawler" User-agent
-
-            for (String robotstxtLine : lines) {
-                String trimmedLine = robotstxtLine.trim();
-
-                // 检查是否有特定User-agent规则
-                if (trimmedLine.equalsIgnoreCase("User-agent: cis5550-crawler")) {
-                    specificRules = true;
-                    continue;
-                } else if (specificRules && trimmedLine.startsWith("Disallow: ")) {
-                    disallowRules.add(trimmedLine.substring("Disallow: ".length()));
-                } else if (specificRules && trimmedLine.startsWith("Allow: ")) {
-                    allowRules.add(trimmedLine.substring("Allow: ".length()));
-                } else if (trimmedLine.equalsIgnoreCase("User-agent: *")) {
-                    specificRules = false; // 恢复到通配符(*)规则
-                } else if (specificRules && trimmedLine.startsWith("Crawl-delay: ")) {
-                    delay = (trimmedLine.substring("Crawl-delay: ".length()));
+                while ((line = reader.readLine()) != null) {
+                    robotstxtContent.append(line).append("\n");
                 }
-            }
-            String disallowString = "";
-            if (!disallowRules.isEmpty()) {
-                disallowString = String.join(",", disallowRules); // 没有 Disallow 规则
-            }
-            String allowString = "";
-            if (!allowRules.isEmpty()) {
-                allowString = String.join(",", allowRules); // 没有 Disallow 规则
-            }
+                reader.close();
 
-            // 返回 Disallow 和 Allow 规则
-            return new String[] { disallowString, allowString, delay };
-        } else {
-            return null; // 没有 robots.txt 文件
-        }
-    	}catch (Exception e) {
+                // 解析 robots.txt 内容
+                String[] lines = robotstxtContent.toString().split("\n");
+                List<String> disallowRules = new ArrayList<>();
+                List<String> allowRules = new ArrayList();
+                String delay = "1";
+                boolean specificRules = false; // 是否有特定的规则适用于 "cis5550-crawler" User-agent
+
+                for (String robotstxtLine : lines) {
+                    String trimmedLine = robotstxtLine.trim();
+
+                    // 检查是否有特定User-agent规则
+                    if (trimmedLine.equalsIgnoreCase("User-agent: cis5550-crawler")) {
+                        specificRules = true;
+                        continue;
+                    } else if (specificRules && trimmedLine.startsWith("Disallow: ")) {
+                        disallowRules.add(trimmedLine.substring("Disallow: ".length()));
+                    } else if (specificRules && trimmedLine.startsWith("Allow: ")) {
+                        allowRules.add(trimmedLine.substring("Allow: ".length()));
+                    } else if (trimmedLine.equalsIgnoreCase("User-agent: *")) {
+                        specificRules = false; // 恢复到通配符(*)规则
+                    } else if (specificRules && trimmedLine.startsWith("Crawl-delay: ")) {
+                        delay = (trimmedLine.substring("Crawl-delay: ".length()));
+                    }
+                }
+                String disallowString = "";
+                if (!disallowRules.isEmpty()) {
+                    disallowString = String.join(",", disallowRules); // 没有 Disallow 规则
+                }
+                String allowString = "";
+                if (!allowRules.isEmpty()) {
+                    allowString = String.join(",", allowRules); // 没有 Disallow 规则
+                }
+
+                // 返回 Disallow 和 Allow 规则
+                return new String[] { disallowString, allowString, delay };
+            } else {
+                return null; // 没有 robots.txt 文件
+            }
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-    	
+
     }
 
     public static String gethost(String[] seedurl1) {
         String host = "";
         if (seedurl1[0] == null) {
-        	System.out.println("  ++++++++++ url missing host："+seedurl1[1]+seedurl1[2]+seedurl1[3]);
-        }
-        else if (seedurl1[2] != null) {
+            System.out.println("  ++++++++++ url missing host：" + seedurl1[1] + seedurl1[2] + seedurl1[3]);
+        } else if (seedurl1[2] != null) {
             host = seedurl1[0] + "://" + seedurl1[1] + ":" + seedurl1[2];
         } else {
             if (seedurl1[0].equals("http")) {
@@ -425,9 +437,9 @@ public class Crawler {
             visitrow.put("delay", rules[2]);
         }
         try {
-        	context.getKVS().putRow("visit-history", visitrow);
-        }catch(Exception e) {
-        	System.out.println(seedurl1[1]+seedurl1[2]+rules[0]+rules[1]);
+            context.getKVS().putRow("visit-history", visitrow);
+        } catch (Exception e) {
+            System.out.println(seedurl1[1] + seedurl1[2] + rules[0] + rules[1]);
             e.printStackTrace();
         }
     }
